e81e910b1be781efbff535d2d4bad3a42d846e06
Software_King@qq.com <xj461352520>
2015-11-04 15:07:35 +0800

    activiti代码初始化

---
+++ activiti-hello/src/main/java/com/xujin/activiti/activiti_hello/App.java
@@ -0,0 +1,13 @@
+package com.xujin.activiti.activiti_hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
